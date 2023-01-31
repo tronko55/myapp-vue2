f5e0af0e69e6a3a764d989066e7100b03918d814
tronko55 <stefalaina@libero.it>
2023-01-31 10:44:36 +0100

    search by ingredient working

--- src/views/IngredientsView.vue
+++ src/views/IngredientsView.vue
@@ -1,117 +1,138 @@
+<!-- con questo codice visualizzo venti elementi per volta ed ho un bottone per caricarne di più.
+non ho quindi integrato il filtro -->
+
 <!-- <template>
-  <div>
-      <md-progress-bar v-show="loading" class="md-accent" md-mode="query"></md-progress-bar>
+    <div>
+        <md-progress-bar v-show="loading" class="md-accent" md-mode="query"></md-progress-bar>
+        <div>
+            <h1>Ingredients</h1>
+        </div>
 
-      <div class="md-layout md-gutter md-alignment-center">
+        <div class="md-layout md-gutter md-alignment-center">
 
-          <div class="md-card" v-for="ingredient in ingredients" :key="ingredient.strIngredient">
-              <router-link :to="{ name: 'ingredient', params: { ingredient: ingredient.strIngredient } }">
-                  <md-card md-with-hover>
-                      <md-card-media-cover md-text-scrim>
-                          <md-card-media md-ratio="16/9">
-                              <img :src="ingredient.strIngredientThumb">
-                          </md-card-media>
+            <div class="md-card" v-for="ingredient in ingredients.slice(0, limit)" :key="ingredient.strIngredient">
+                <router-link :to="{ name: 'ingredient', params: { ingredient: ingredient.strIngredient } }">
+                    <md-card md-with-hover>
+                        <md-card-media-cover md-text-scrim>
+                            <md-card-media md-ratio="16/9">
+                                <img
+                                    :src="'https://www.themealdb.com/images/ingredients/' + ingredient.strIngredient + '.png'">
+                            </md-card-media>
 
-                          <md-card-area>
-                              <md-card-header>
-                                  <span class="md-title">{{ ingredient.strIngredient }}</span>
-                              </md-card-header>
-
-                              <md-card-actions>
-                          <md-button class="md-icon-button">
-                              <md-icon>favorite</md-icon>
-                          </md-button>
-
-                      </md-card-actions> 
-
-                          </md-card-area>
-                      </md-card-media-cover>
-                  </md-card>
-              </router-link>
-          </div>
-      </div>
-  </div>
+                            <md-card-area>
+                                <md-card-header>
+                                    <span class="md-title">{{ ingredient.strIngredient }}</span>
+                                </md-card-header>
+                            </md-card-area>
+                        </md-card-media-cover>
+                    </md-card>
+                </router-link>
+            </div>
+        </div>
+        <div>
+            <md-button class="md-raised md-primary" @click="loadMore">Load More</md-button>
+        </div>
+    </div>
 </template>
-  
+
 <script>
 import axiosApi from '@/axiosApi';
 
 export default {
-  data: function () {
-      return {
-          ingredients: {},
-          mealsList: [],
-          loading: true
-      }
-  },
-  mounted: function () {
-      axiosApi.getIngredients().then((result) => {
-          console.log(result.data.meals);
-          this.ingredients = result.data.ingredients;
-          this.loading = false
-      });
-  },
+    data() {
+        return {
+            keyword: "",
+            ingredients: [],
+            loading: true,
+            limit: 20
+        }
+    },
+    methods: {
+        loadMore: function () {
+            this.limit += 20;
+            this.loadCards()
 
+        },
+        loadCards: function () {
+            axiosApi.getIngredients().then(({ data }) => {
+                console.log(data);
+                this.ingredients = data.meals.slice(0, this.limit);
+                this.loading = false;
+            });
+        }
+    },
+    mounted: function () {
+        this.loadCards()
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 .md-card {
-  width: 320px;
-  margin: 4px;
-  display: inline-block;
-  vertical-align: top;
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
 
 }
 </style> -->
 
+
+<!-- In questo template invece non visualizzo gli elementi in modo coerente con le categorie, 
+    ma siccome non vedo le immagini allora ha bisogno di meno tempo per caricare e posso usare i filtri su una lista di tutti gli ingredienti -->
 <template>
     <div>
-        <div class="p-8 pb-0">
-            <h1 class="text-4xl font-bold mb-4 text-orange-500">Ingredients</h1>
+        <div>
+            <h1>Ingredients</h1>
         </div>
-        <div class="px-8">
-            <input type="text" v-model="keyword"
-                class="rounded border-2 bg-white border-gray-200 focus:ring-orange-500 focus:border-orange-500 mb-3 w-full"
-                placeholder="Search for Ingredients" />
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
-                <a v-for="ingredient of computedIngredients" :key="ingredient.idIngredient"
-                    class="block bg-white rounded p-3 mb-3 shadow">
+        <div>
+            <md-input type="text" v-model="keyword" placeholder="Search for Ingredients">
+            </md-input>
+        </div>
 
-                    <router-link :to="{ name: 'ingredient', params: { ingredient: ingredient.strIngredient } }">
-                        <h3 class="text-2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
-                    </router-link>
-                </a>
-            </div>
-        </div>
+        <md-card md-with-hover v-for="ingredient in filteredIngredients" :key="ingredient.strIngredient">
+            <md-ripple>
+                <router-link :to="{ name: 'ingredient', params: { ingredient: ingredient.strIngredient } }">
+                    <md-card-header>
+                        <div class="md-title">{{
+                            ingredient.strIngredient
+                        }}</div>
+                    </md-card-header>
+                </router-link>
+            </md-ripple>
+        </md-card>
     </div>
+
+
 </template>
 
-<script setup>
-import { computed, onMounted, ref } from "vue";
+
+<script>
 import axiosApi from '@/axiosApi';
 
+export default {
+    data() {
+        return {
+            keyword: "",
+            ingredients: [],
+            loading: true,
 
+        }
+    },
+    computed: {
+        filteredIngredients() {
+            return this.ingredients.filter(i =>
+                i.strIngredient.toLowerCase().includes(this.keyword.toLowerCase())
+            );
+        }
+    },
 
-const keyword = ref("");
-const ingredients = ref([]);
-const computedIngredients = computed(() => {
-    if (!computedIngredients) return ingredients;
-    return ingredients.value.filter((i) =>
-        i.strIngredient.toLowerCase().includes(keyword.value.toLowerCase())
-    );
-});
+    mounted: function () {
 
-// function openIngredient(ingredient) {
-//     // store.commit('setIngredient', ingredient)
-//     router.push({
-//         name: "byIngredient",
-//         params: { ingredient: ingredient.strIngredient },
-//     });
-// }
-onMounted(() => {
-    axiosApi.getIngredients().then(({ data }) => {
-        ingredients.value = data.meals;
-    });
-});
+        axiosApi.getIngredients().then(({ data }) => {
+            this.ingredients = data.meals;
+            console.log(this.ingredients)
+        })
+    }
+}
 </script>
